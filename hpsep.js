818867c0745d78e953365c7591f170a9c199133f
obaldia <deobaldia@hsu-hh.de>
2018-10-16 16:02:18 +0200

    First commit of a test script for tracking

---
+++ hpsep.js
@@ -0,0 +1,2 @@
+/* This file is a port of a harmonic/percussive 
+separation based on the code by Fitzgerald et al. */
---
+++ spec.js
@@ -0,0 +1,3 @@
+/* This file is a test implementation for a pitch tracker,
+based on phase locked loops. */
+
